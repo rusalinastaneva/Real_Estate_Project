91cffe47b4b9907edd3acdeb7eee0a50024e3e40
Rusalina Staneva <rossavelrs@yahoo.com>
2020-08-24 17:23:15 -0500

    Inital commit

--- rep/settings.py
+++ rep/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f#vv$r@%84qgg!vou6vd9l%g3#o*k1@8=_(t)$!zq-w&cefm($'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'redb',
-        'USER': 'postgres',
+        'USER': '',
         'PASSWORD': '',
         'HOST': 'localhost'
     }
